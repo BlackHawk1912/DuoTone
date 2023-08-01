483afa9793ea961249ea4c75ba238d6b47b149bc
BlackHawk1912 <xBlackHawk1912x@gmail.com>
2023-08-01 23:38:32 +0200

    first pwa test

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,25 +1,22 @@
-const staticDevCoffee = "dev-coffee-site-v1"
-const assets = [
-  "/",
-  "/index.html",
-  "/style.css",
-  "/script.js",
-  "/background.jpg",
-  "/slockscreen.jpeg"
+var GHPATH = '/DuoTone';
+var APP_PREFIX = 'duotonepwa_';
+
+// The version of the cache. Every time you change any of the files
+// you need to change this version (version_01, version_02…). 
+// If you don't change the version, the service worker will give your
+// users the old files!
+var VERSION = 'version_00';
+
+// The files to make available for offline use. make sure to add 
+// others to this list
+var URLS = [
+  `${GHPATH}/`,
+  `${GHPATH}/background.jpg`,
+  `${GHPATH}/duotone.js`,
+  `${GHPATH}/duotone.svg`,
+  `${GHPATH}/index.html`,
+  `${GHPATH}/style.css`,
+  `${GHPATH}/script.js`,
+  `${GHPATH}/lockscreen.png`,
+  `${GHPATH}/manifest.json`
 ]
-
-self.addEventListener("install", installEvent => {
-  installEvent.waitUntil(
-    caches.open(staticDevCoffee).then(cache => {
-      cache.addAll(assets)
-    })
-  )
-})
-
-self.addEventListener("fetch", fetchEvent => {
-  fetchEvent.respondWith(
-    caches.match(fetchEvent.request).then(res => {
-      return res || fetch(fetchEvent.request)
-    })
-  )
-})
