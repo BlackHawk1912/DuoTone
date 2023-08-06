a9d53355f7056b55d1e36db7840a102630971eca
Simon von Schnakenburg <simon.schnakenburg@bender.de>
2023-08-06 22:39:48 +0200

    next try...

--- serviceWorker.js
+++ serviceWorker.js
@@ -26,6 +26,25 @@ if (workbox.navigationPreload.isSupported()) {
 }
 
 self.addEventListener("fetch", (event) => {
+  const url = new URL(event.request.url);
+  if (
+    event.request.method === "POST" &&
+    url.pathname === "/" &&
+    url.searchParams.has("share-target")
+  ) {
+    event.respondWith(Response.redirect("/?receiving-file-share=1"));
+
+    event.waitUntil(
+      (async function () {
+        const client = await self.clients.get(event.resultingClientId);
+        const data = await event.request.formData();
+        const files = data.get("file");
+        client.postMessage({ files });
+      })()
+    );
+    return;
+  }
+
   if (event.request.mode === "navigate") {
     alert("event.request.mode === navigate");
     event.respondWith(
